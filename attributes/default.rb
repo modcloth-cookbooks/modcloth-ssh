0a39e819ba50654d1be1675736614a651424bf77
Seth Kingry <s.kingry@modcloth.com>
2013-05-14 21:06:51 +0000

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: ssh
+# Attributes:: default
+#
+# Copyright ModCloth, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "ModCloth, Inc."
+maintainer_email "platformsphere@modcloth.com"
+license          "All Rights Reserved"
+description      "Configures SSH"
+version          "0.1.0"
+
